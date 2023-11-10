18dc3976f9a86d48b5b5062281858a8e233cd8e2
1Samoth <thomasstamand1@gmail.com>
2023-11-10 13:25:23 -0500

    Vérification Ajout Propriétaire

--- Exercice SQL/Exercice SQL/AjouterProprio.xaml.cs
+++ Exercice SQL/Exercice SQL/AjouterProprio.xaml.cs
@@ -27,5 +27,65 @@ namespace Exercice_SQL
         {
             this.InitializeComponent();
         }
+
+        private void btnAjouter_Click(object sender, RoutedEventArgs e)
+        {
+            string id = "";
+            string prenom = "";
+            string nom = "";
+            bool valide = true;
+
+            if(tbxID.Text == "")
+            {
+                tblIDErr.Text = "Veuillez entrer un ID";
+                valide = false;
+            }
+            else if(tbxID.Text.Length > 10){
+                tblIDErr.Text = "L'ID ne peut pas dépasser 10 caractères";
+                valide = false;
+            }
+            else
+            {
+                tblIDErr.Text = "";
+                id = tbxID.Text;
+            }
+
+            if(tbxPrenom.Text == "")
+            {
+                tblPrenomErr.Text = "Veuillez entrer un prénom";
+                valide = false;
+            }
+            else if(tbxPrenom.Text.Length > 20) 
+            {
+                tblPrenomErr.Text = "Le prénom ne peut pas dépasser 20 caractères";
+                valide = false;
+            }
+            else
+            {
+                tblPrenomErr.Text = "";
+                prenom = tbxPrenom.Text;
+            }
+
+            if(tbxNom.Text == "")
+            {
+                tblNomErr.Text = "Veuillez entrer un nom";
+                valide = false;
+            }
+            else if(tbxNom.Text.Length > 20)
+            {
+                tblNomErr.Text = "Le nom ne peut pas dépasser 20 caractères";
+                valide = false;
+            }
+            else
+            {
+                tblNomErr.Text = "";
+                nom = tbxNom.Text;
+            }
+
+            if (valide)
+            {
+
+            }
+        }
     }
 }
